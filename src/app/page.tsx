2c7c3f0e983b7726df936ac8a5ec95300e26b822
MOHSIN ALI <mrmohsin13p.com>
2024-02-29 21:54:19 +0500

    Files Added

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen  bg-black/[0.96] bg-grid-white[0.02] antialiased ">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
+
+    </main>
+  );
+}
